7232784f2d8845ba247b84eb96519b58f31ba0c9
Bykova Kateryna <bykova.kateryna@gmail.com>
2022-07-23 15:33:34 +0300

    HW8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,26 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallery = document.querySelector(".gallery");
+
+const createGalleryItems = createGarery(galleryItems);
+
+function createGarery (galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>`
+    }).join("")
+}
+
+gallery.insertAdjacentHTML("beforeend", createGalleryItems)
+
+const lightbox = new SimpleLightbox(".gallery a", {
+        captionsData: "alt",
+        captionDelay: 250,
+    });
+
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+const throttle = require('lodash.throttle');
 
+const form = document.querySelector(".feedback-form")
+
+const LOCALSTORAGE_KEY = "feedback-form-state";
+
+form.addEventListener("input", throttle(savedAddInput, 500));
+form.addEventListener("submit", submitForm);
+
+const formData = {};
+updateInput ();
+
+function savedAddInput(event){
+    event.preventDefault();
+    formData[event.target.name] = event.target.value;
+  
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData))
+
+}
+
+function submitForm (event) {
+    event.preventDefault();
+
+    const { email, message } = event.target;
+    const formData = {email: email.value, message: message.value}
+    console.log(formData)
+    localStorage.removeItem(LOCALSTORAGE_KEY)
+    form.reset();
+}
+
+
+function updateInput () {
+    if (localStorage.getItem(LOCALSTORAGE_KEY) === null) {
+        return;
+    }
+    auditInput = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+    form.elements.email.value = auditInput.email;
+    form.elements.message.value = auditInput.message;
+}
